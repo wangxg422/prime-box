84e7e35cc6d7c29814702eb309b3fa437e03b3a7
wangxg <wangxg422@outlook.com>
2025-07-01 19:55:01 +0800

    install tailwindcss

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,19 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import path from 'node:path';
+import tailwindcss from '@tailwindcss/vite';
 
-// @ts-expect-error process is a nodejs global
+// //@ts-expect-error process is a nodejs global
 const host = process.env.TAURI_DEV_HOST;
 
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
-  plugins: [vue()],
+  plugins: [vue(), tailwindcss()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
   //
