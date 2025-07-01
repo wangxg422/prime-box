91ac478306e95a8c58be10312da83aebe3610a8d
wangxg <wangxg422@outlook.com>
2025-07-01 21:13:22 +0800

    add resizable

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,22 @@
 <script setup lang="ts">
-import { Button } from '@/components/ui/button';
-import { Switch } from '@/components/ui/switch'
-
-import { ref } from 'vue'
-import { cn } from '@/lib/utils'
-import { Slider } from '@/components/ui/slider'
-
-const modelValue = ref([50])
-
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from '@/components/ui/resizable'
 </script>
 
 <template>
   <main class="container">
-    <div>
-      <Button>Click me</Button>
-    </div>
-    <div>
-      <Slider v-model="modelValue" :max="100" :step="1" :class="cn('w-3/5', $attrs.class ?? '')" />
-    </div>
-    <div>
-      <Switch />
-    </div>
+    <ResizablePanelGroup id="demo-group-1" direction="horizontal" class="w-full h-screen">
+      <ResizablePanel id="demo-panel-1" :default-size="30" :max-size="40" :min-size="20">
+        Left
+      </ResizablePanel>
+      <ResizableHandle id="demo-handle-1" />
+      <ResizablePanel id="demo-panel-2"  >
+        Right
+      </ResizablePanel>
+    </ResizablePanelGroup>
   </main>
 </template>
 
@@ -32,9 +28,6 @@ const modelValue = ref([50])
   line-height: 24px;
   font-weight: 400;
 
-  color: #0f0f0f;
-  background-color: #f6f6f6;
-
   font-synthesis: none;
   text-rendering: optimizeLegibility;
   -webkit-font-smoothing: antialiased;
@@ -45,26 +38,7 @@ const modelValue = ref([50])
 .container {
   margin: 0;
   padding: 0;
-}
-
-@media (prefers-color-scheme: dark) {
-  :root {
-    color: #f6f6f6;
-    background-color: #2f2f2f;
-  }
-
-  a:hover {
-    color: #24c8db;
-  }
-
-  input,
-  button {
-    color: #ffffff;
-    background-color: #0f0f0f98;
-  }
-
-  button:active {
-    background-color: #0f0f0f69;
-  }
+  width: 100%;
+  height: 100vh;
 }
 </style>
