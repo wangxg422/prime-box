4551f9210346c893ef819e524d2f2c0c016b34db
wangxg422 <wangxg422@outlook.com>
2025-07-02 09:58:22 +0800

    add vue-router

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,11 @@
 <script setup lang="ts">
-import {
-  ResizableHandle,
-  ResizablePanel,
-  ResizablePanelGroup,
-} from '@/components/ui/resizable'
+import Main from "@/view/main/main.vue";
+
 </script>
 
 <template>
   <main class="container">
-    <ResizablePanelGroup id="demo-group-1" direction="horizontal" class="w-full h-screen">
-      <ResizablePanel id="demo-panel-1" :default-size="30" :max-size="40" :min-size="20">
-        Left
-      </ResizablePanel>
-      <ResizableHandle id="demo-handle-1" />
-      <ResizablePanel id="demo-panel-2"  >
-        Right
-      </ResizablePanel>
-    </ResizablePanelGroup>
+    <Main />
   </main>
 </template>
 
---
+++ src/view/main/main.vue
@@ -0,0 +1,20 @@
+<template>
+    <ResizablePanelGroup id="demo-group-1" direction="horizontal" class="w-full h-screen">
+        <ResizablePanel id="demo-panel-1" :default-size="30" :max-size="40" :min-size="20">
+        </ResizablePanel>
+        <ResizableHandle id="demo-handle-1" />
+        <ResizablePanel id="demo-panel-2">
+            Right
+        </ResizablePanel>
+    </ResizablePanelGroup>
+</template>
+
+<script setup lang="ts">
+import {
+    ResizableHandle,
+    ResizablePanel,
+    ResizablePanelGroup,
+} from '@/components/ui/resizable'
+</script>
+
+<style scoped></style>
